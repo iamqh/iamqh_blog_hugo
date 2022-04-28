c0cadc88080c702f70844185a145422b65c22c46
iamqh <hoapq.4398@gmail.com>
2022-04-28 21:51:09 +0700

    update port deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop iamqh_blog
 docker rm iamqh_blog
-docker run -d -e VIRTUAL_HOST=blog.iamqh.me --name iamqh_blog hugo-app:latest
+docker run -d -e VIRTUAL_HOST=blog.iamqh.me -p 2909:80 --name iamqh_blog hugo-app:latest
